670415d25a1563767b315c2e51bb50154dbacfa3
Merugu Vaishnavi <vaishnavimerugu2005@gmail.com>
2025-09-11 19:02:48 +0530

    Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,5 +2,7 @@ package hello;
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
+    System.out.println("From Vaishnavi");
+    
   }
 }
