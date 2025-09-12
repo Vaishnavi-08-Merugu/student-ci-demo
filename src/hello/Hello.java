14fea13a367d87f56c9addf82ae439158428fbbe
Merugu Vaishnavi <vaishnavimerugu2005@gmail.com>
2025-09-12 19:15:17 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
     System.out.println("From Vaishnavi");
+    System.out.println("From Vaishnavi webhook");
     
   }
 }
