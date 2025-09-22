02e28e666f9927f857091bedab4860c56499377f
Merugu Vaishnavi <vaishnavimerugu2005@gmail.com>
2025-09-22 11:17:40 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
     System.out.println("From Vaishnavi web");
      System.out.println("From Vaishnavi webs");
     System.out.println("From Vaishnavi Pool scm");
+    System.out.println("From Vaishnavi Pool scms");
   }
 }
