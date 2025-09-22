bf43fae2e6db24dedb783190efd9bad81c4579ce
Merugu Vaishnavi <vaishnavimerugu2005@gmail.com>
2025-09-22 10:35:02 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
     System.out.println("From Vaishnavi webhooks");
     System.out.println("From Vaishnavi webhooks");
     System.out.println("From Vaishnavi web");
+     System.out.println("From Vaishnavi webs");
   }
 }
